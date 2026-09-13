2f794295eaa86b31e7ca704039d22b4a595c9328
Aditya-linux <adityanishad0402@gmail.com>
2026-09-13 23:08:41 +0530

    fix(ci): exclude Hardhat tests from Jest, fix express-rate-limit version to v8

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: '@react-native/jest-preset',
   setupFiles: ['./jest.setup.js'],
+  testPathIgnorePatterns: ['/node_modules/', '<rootDir>/test/'],
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?|@react-navigation)/)',
   ],

8dbfac4c8bcaed9ed750d3939f1ae7e3574165da
Aditya-linux <adityanishad0402@gmail.com>
2026-09-18 00:03:36 +0530

    chore: fix jest config for supabase imports

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,6 @@ module.exports = {
   setupFiles: ['./jest.setup.js'],
   testPathIgnorePatterns: ['/node_modules/', '<rootDir>/test/'],
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?|@react-navigation|@react-native-async-storage)/)',
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?|@react-navigation|@react-native-async-storage|react-native-url-polyfill|@supabase/supabase-js)/)',
   ],
 };

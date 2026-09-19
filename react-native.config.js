903013664afdd5b45978319fc75ba213155a9c71
DubeyJi03 <dubeyanurag3925@gmail.com>
2026-09-19 11:56:58 +0530

    chore(config): initialize project configuration, tooling and dependency manifests

---
+++ react-native.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  dependencies: {
+    'react-native-hce': {
+      platforms: {
+        android: null, // Native source integrated directly into android/app for AGP & Gradle 9 compatibility
+      },
+    },
+  },
+};
